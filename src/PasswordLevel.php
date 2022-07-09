c4ecfb43145eb9467cbb663e051771dea1d52803
Gbenankpon Arthur <arth.bage@gmail.com>
2022-07-09 20:09:33 +0100

    fix

--- src/PasswordLevel.php
+++ src/PasswordLevel.php
@@ -19,7 +19,7 @@ class PasswordLevel {
 		$level += (int)($number && $uppercase && $lowercase); // level 4 or 5
 		$level += (int)($level > 1 && $symbol); // level 3, 4, 5 or 6
 	
-		$level += (int)!(strlen($password) > 8); // level 2, 3, 4, 5 or 6
+		$level += (int)!!(strlen($password) > 8); // level 2, 3, 4, 5 or 6
 		return $level;
 	}
 	
