94ecb4a3f530113a8383a96bb427b682db458824
Gbenankpon Arthur <arth.bage@gmail.com>
2022-07-09 12:04:05 +0100

    added class

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5fd416c8653cc42ba77485cb6e7ebf30
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5fd416c8653cc42ba77485cb6e7ebf30', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5fd416c8653cc42ba77485cb6e7ebf30', 'loadClassLoader'));
